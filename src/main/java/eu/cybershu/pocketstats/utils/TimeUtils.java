49af0938effe9d0f4777204cf05a61838da45928
Michał Mazur <michmzr@gmail.com>
2023-01-26 17:30:35 +0700

    Updated raporting

--- src/main/java/eu/cybershu/pocketstats/utils/TimeUtils.java
+++ src/main/java/eu/cybershu/pocketstats/utils/TimeUtils.java
@@ -1,6 +1,9 @@
 package eu.cybershu.pocketstats.utils;
 
-import java.time.*;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 public class TimeUtils {
     public static Instant instantTodayBegin() {
@@ -13,21 +16,5 @@ public class TimeUtils {
         return OffsetDateTime
                 .now(ZoneOffset.UTC)
                 .with(LocalTime.of(23, 59, 59)).toInstant();
-    }
-
-    public static LocalDateTime dayBegin(LocalDateTime localDateTime) {
-        return localDateTime.withHour(0).withMinute(0).withSecond(1);
-    }
-
-    public static LocalDateTime dayEnd(LocalDateTime localDateTime) {
-        return localDateTime.withHour(23).withMinute(59).withSecond(59);
-    }
-
-    public static Instant localDateTimeToInstant(LocalDateTime ldt) {
-        return ldt.atZone(getZoneId()).toInstant();
-    }
-
-    private static ZoneId getZoneId() {
-        return ZoneId.systemDefault();
     }
 }
