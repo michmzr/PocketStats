5a1550032e16f39a5e6b6b67eba14cb94658a518
Michał Mazur <michmzr@gmail.com>
2023-02-10 14:31:44 +0700

    Refactorings Added extra stats - tags

--- src/main/java/eu/cybershu/pocketstats/controller/PocketApiController.java
+++ src/main/java/eu/cybershu/pocketstats/controller/PocketApiController.java
@@ -2,7 +2,6 @@ package eu.cybershu.pocketstats.controller;
 
 import eu.cybershu.pocketstats.pocket.PocketAuthorizationService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pocket/auth/")
 public class PocketApiController {
-    @Autowired
-    private PocketAuthorizationService authorizationService;
+    private final PocketAuthorizationService authorizationService;
+
+    public PocketApiController(PocketAuthorizationService authorizationService) {
+        this.authorizationService = authorizationService;
+    }
 
     @GetMapping("token")
     public String onRedirect() {
-        log.info("Redirection successfull");
+        log.info("Redirection successful.");
 
         authorizationService.deregisterAuthSession();
 
