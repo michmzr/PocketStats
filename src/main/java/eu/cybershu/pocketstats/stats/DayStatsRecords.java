287a2629ce3cf778451de8bc036b8e6fc34785e3
Michał Mazur <michmzr@gmail.com>
2023-04-13 13:01:23 +0800

    Updated app version

--- src/main/java/eu/cybershu/pocketstats/stats/DayStatsRecords.java
+++ src/main/java/eu/cybershu/pocketstats/stats/DayStatsRecords.java
@@ -3,7 +3,6 @@ package eu.cybershu.pocketstats.stats;
 import java.util.List;
 
 public record DayStatsRecords(
-        List<DayStat> stats,
-        DayStatsType type
+        List<DayStat> stats, DayStatsType type
 ) {
 }
