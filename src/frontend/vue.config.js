e7b67bdbcd241925253a2b27fd8c824896a15153
Michał Mazur <michmzr@gmail.com>
2023-05-10 11:32:04 +0800

    Set public path in vue

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -7,5 +7,8 @@ module.exports = defineConfig({
 module.exports = {
   devServer: {
     port: 8080
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/pocketstats/'
+      : '/'
 };
