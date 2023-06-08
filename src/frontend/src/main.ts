059652b3959c49dc92db47984dd5abbb263e0a78
Mike <michmzr@gmail.com>
2023-06-08 23:50:57 +0800

    #15 Auto refresh page when user authorized with pocket or resynced #15 (#18)
    - Changed how tags stats look like, small fixes
     - Autorefresh stats components when sync was made

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -6,7 +6,6 @@ import router from "@/router";
 import {createPinia} from "pinia";
 import {BootstrapVue3, BToastPlugin} from "bootstrap-vue-3";
 
-// Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
@@ -14,11 +13,13 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {faSpinner, faUser} from "@fortawesome/free-solid-svg-icons";
 
+//font awesome
 library.add(faSpinner, faUser);
 
 const app = createApp(App)
 const pinia = createPinia()
 
+// @ts-ignore
 app
     .use(pinia)
     .use(VueAxios, axios)
@@ -27,3 +28,4 @@ app
     .use(BToastPlugin)
     .component("font-awesome-icon", FontAwesomeIcon)
     .mount('#app')
+
